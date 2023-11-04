d40444b7ce208b8c0d727c6691e4e1a4a10d3406
Vihan Perera <vihanpereraux@gmail.com>
2023-11-04 11:19:31 +0000

    Add client and server endpoints

--- assets/js/muse_connection.js
+++ assets/js/muse_connection.js
@@ -4,7 +4,14 @@ import { MuseClient } from 'muse-js';
 let accelX = 0;
 let accelY = 0;
 let accelZ = 0;
+
+let elec1 = 0;
+let elec2 = 0;
+
 const connectorbutton = document.getElementById('connector');
+
+let getDataButtonClicked = false;
+let getRequestInterval;
 
 async function main() {
   let client = new MuseClient();
@@ -36,11 +43,28 @@ osc(20, 0.01, 1.1)
 	.kaleid(10)
 	.color(10.83,0.91,0.39)
 	.rotate(0, 0.1)
-	// .modulate(o0, () => (accelX * 1000) * 0.0003)
-  .modulate(o0, () => mouseXCord * 0.0003)
+	.modulate(o0, () => (elec1 * 500) * 0.0003)
+  // .modulate(o0, () => mouseXCord * 0.0003)
     .modulate(
       src(o0)
-      .modulate(noise(3),() => mouseXCord * 0.0003)
+      .modulate(noise(3),() => (elec1 * 1000) * 0.0003)
     )
 	  .scale(1.01)
   .out(o0)
+
+  document.getElementById('get-data').addEventListener('click', function(){
+    if(!getDataButtonClicked){
+      getDataButtonClicked = true;
+      getRequestInterval = setInterval(async () => {
+        const response = await fetch('http://127.0.0.1:5000/');
+        const myJson = await response.json(); //extract JSON from the http response
+
+        elec1 = myJson.elec1
+        elec2 = myJson.elec2
+      }, 100);
+    }
+    else{
+      getDataButtonClicked = false;
+      clearInterval(getRequestInterval);
+    }
+  });
